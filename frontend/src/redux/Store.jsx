94c31df311b0f0938bd6a7c41c37ca0464ffa3d8
afnan <afnanballoor44@gmail.com>
2024-02-09 17:25:05 +0530

    1. updated store.jsx  2. added loggedUser & loggedOutUser middlewares

--- frontend/src/redux/Store.jsx
+++ frontend/src/redux/Store.jsx
@@ -1,9 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { alertSlice } from "./AlertSlice";
+import { AuthSlice } from './AuthSlice'
+import { ArtistAuthSlice } from './ArtistAuthSlice'
 
 const store = configureStore({
     reducer:{
         alerts:alertSlice.reducer,
+        Auth:AuthSlice,
+        ArtistAuth:ArtistAuthSlice,
     },
 })
 
